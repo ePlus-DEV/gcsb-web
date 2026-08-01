c2929b3e7127aa8b049521d2bdfea579394ef4e9
David Nguyen <h250694@gmail.com>
2026-08-01 19:48:21 +0700

    ci: support static exports for Pages previews

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
+const isStaticExport = process.env.NEXT_STATIC_EXPORT === "true"
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || ""
+
 const nextConfig = {
+  ...(isStaticExport
+    ? {
+        output: "export",
+        trailingSlash: true,
+        basePath,
+      }
+    : {}),
   images: {
     unoptimized: true,
   },
