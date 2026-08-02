91820a2284a4f6cff7809a7263a225e08068e3fa
David Nguyen <h250694@gmail.com>
2026-08-02 07:45:38 +0700

    fix: load Firefox Browser icon styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import "./styles/redesign-results.css"
 import "./styles/redesign-components.css"
 import "./styles/redesign-responsive.css"
 import "./styles/facilitator-panel.css"
+import "./styles/firefox-icon.css"
 
 const siteUrl = "https://arcade.eplus.dev/"
 const siteName = "Arcade Points by ePlus.DEV"
