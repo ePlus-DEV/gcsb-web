3bc9704c5abff76c5f5cac96a6c337a2dccd9531
David Nguyen <h250694@gmail.com>
2026-08-03 11:55:11 +0700

    Sync browser theme color with selected theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,10 +31,6 @@ const fontAwesomeIntegrity =
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "#f4f7ff" },
-    { media: "(prefers-color-scheme: dark)", color: "#050918" },
-  ],
   colorScheme: "light dark",
 }
 
